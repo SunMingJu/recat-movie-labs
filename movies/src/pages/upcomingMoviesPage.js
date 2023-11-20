329a53a2e28ab3d643e00a92ce299c5bc90a76b8
MingJu Sun <20104599@mail.wit.ie>
2023-11-20 00:03:17 +0000

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import Spinner from '../components/spinner';
 import { useQuery } from 'react-query';
-import AddToFavoritesIcon  from "../components/cardIcons/addToFavorites";
+import PlaylistAddIcon from '../components/cardIcons/playlistAdd'
 
 const UpcomingMoviesPage = () => {
 
@@ -28,7 +28,7 @@ const UpcomingMoviesPage = () => {
       title="Upcoming Movies"
           movies={movies}
           action={(movie) => {
-            return <AddToFavoritesIcon movie={movie} />
+            return <PlaylistAddIcon movie={movie} />
       }}    
     />
   );
